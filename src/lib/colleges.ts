5cdd65804e56ec01f133cc1dc3bdeed64ef7e308
Divyam Aggarwal <divyamagg2005@gmail.com>
2025-05-30 19:02:01 +0000

    add more colleges like vellore institute of technology and etc

--- src/lib/colleges.ts
+++ src/lib/colleges.ts
@@ -11,6 +11,15 @@ export const colleges: College[] = [
   { id: "caltech", name: "California Institute of Technology" },
   { id: "ucla", name: "University of California, Los Angeles" },
   { id: "berkeley", name: "University of California, Berkeley" },
+  { id: "vit", name: "Vellore Institute of Technology" },
+  { id: "iitb", name: "Indian Institute of Technology Bombay" },
+  { id: "iitd", name: "Indian Institute of Technology Delhi" },
+  { id: "iitm", name: "Indian Institute of Technology Madras" },
+  { id: "iitk", name: "Indian Institute of Technology Kanpur" },
+  { id: "iitkgp", name: "Indian Institute of Technology Kharagpur" },
+  { id: "bits", name: "Birla Institute of Technology and Science, Pilani" },
+  { id: "srm", name: "SRM Institute of Science and Technology" },
+  { id: "manipal", name: "Manipal Academy of Higher Education" },
 ];
 
 export const getCollegeById = (id: string): College | undefined => {
